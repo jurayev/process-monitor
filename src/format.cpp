aab06ce3acdfd916e4a0d4d8a4ac04ce61d5c031
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2020-07-04 11:35:28 +0200

    refactor: Improve readability, update calculations, rename vars

--- src/format.cpp
+++ src/format.cpp
@@ -2,21 +2,13 @@
 
 #include <string>
 
-using std::string;
-
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-string Format::ElapsedTime(long seconds) {
-  // time_t elapsed_time = seconds;
-  // tm *elapsed_tm;
-  // elapsed_tm = localtime(&elapsed_time);
-  // char buffer[100];
-  // strftime(buffer, sizeof(buffer), "%H:%M:%S", elapsed_tm);
-  // return buffer;
+std::string Format::Time(long time) {
   char buffer[100];
-  int m = seconds % 3600 / 60;
-  int h = seconds / 3600;
-  int s = seconds % 3600 % 60;
-  sprintf(buffer, "%02d:%02d:%02d", h, m, s);
+  int hours = time / (60 * 60);
+  int minutes = (time / 60) % 60;
+  int seconds = time % 60;
+  sprintf(buffer, "%02d:%02d:%02d", hours, minutes, seconds);
   return buffer;
 }
